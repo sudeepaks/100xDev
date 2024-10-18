56d16b8b33912842cba5ca0639b9de58d2562ed2
sudeepaks <92206271+sudeepaks@users.noreply.github.com>
2024-10-18 10:47:01 +0530

    started new project

---
+++ Gmail-clone/client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/shared/Navbar'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
